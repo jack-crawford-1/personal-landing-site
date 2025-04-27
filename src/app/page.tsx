17057e0fbe510cf74a35678f45bdcbff13e9c261
Jack Crawford <primes_unit.05@icloud.com>
2025-04-28 02:28:12 +1200

    redirect to react native expo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,24 @@
 import IntroText from '../components/IntroText';
 import Hero from '../components/Hero';
 import Project from '../components/Project';
+import { useEffect } from 'react';
 
 export default function Home() {
+  useEffect(() => {
+    const userAgent =
+      typeof navigator === 'undefined' ? '' : navigator.userAgent;
+    const isMobile =
+      /iPhone|iPad|iPod|Android|webOS|BlackBerry|IEMobile|Opera Mini/i.test(
+        userAgent
+      );
+
+    const isSmallScreen = window.innerWidth <= 768;
+
+    if (isMobile || isSmallScreen) {
+      window.location.href = 'https://www.olddogonline.com/';
+    }
+  }, []);
+
   return (
     <>
       <main className="bg-dark text-light items-center flex flex-col sm:items-start  justify-center min-h-screen p-10 md:pl-15 md:pr-15 2xl:pl-55 2xl:pr-55 ubuntu-medium">
