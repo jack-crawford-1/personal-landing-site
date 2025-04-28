9ee8bf445f7ab8496e1dc9189fb3aed1de0723b8
Jack Crawford <primes_unit.05@icloud.com>
2025-04-28 18:15:06 +1200

    fonts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,7 +23,7 @@ export default function Home() {
 
   return (
     <>
-      <main className="bg-dark text-light items-center flex flex-col sm:items-start  justify-center min-h-screen  md:pl-15 md:pr-15 2xl:pl-55 2xl:pr-55 ubuntu-medium pb-20 md:pb-0 pt-5 md:pt-0">
+      <main className="bg-dark text-light items-center flex flex-col sm:items-start  justify-center min-h-screen  md:pl-15 md:pr-15 2xl:pl-55 2xl:pr-55  pb-20 md:pb-0 pt-5 md:pt-0">
         <div className=" p-5 mb-10 xl:pl-15 justify-center items-center flex md:mt-0 mt-10 2xl:mb-20">
           <Hero />
         </div>
