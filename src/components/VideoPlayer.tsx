f1e4f91b8d8671b1c662f4526751cc5883f6541e
Jack Crawford <primes_unit.05@icloud.com>
2025-05-01 00:31:51 +1200

    debugging

--- src/components/VideoPlayer.tsx
+++ src/components/VideoPlayer.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import { useEffect, useRef, useState } from 'react';
-import { motion, AnimatePresence } from 'framer-motion';
 
 type Props = {
   videoSource: string;
@@ -51,40 +50,34 @@ export default function CustomVideoPlayer({
   return (
     <div className="flex flex-col items-center justify-center w-full max-w-5xl mx-auto">
       <div className="relative w-full aspect-video  ">
-        {/* <AnimatePresence>
-          {showPoster && (
-            <motion.img
-              key="poster"
-              src={posterSource}
-              alt="Poster"
-              className="absolute inset-0 w-full h-full object-cover"
-              initial={{ opacity: 1 }}
-              animate={{ opacity: 1 }}
-              exit={{ opacity: 0 }}
-              transition={{ duration: 0.8 }}
-            />
-          )}
-        </AnimatePresence> */}
-
         <video
           key={key}
           ref={videoRef}
           src={videoSource}
           poster={posterSource}
-          onLoadedData={handleReadyToDisplay}
+          // onLoadedData={handleReadyToDisplay}
           className="absolute inset-0 w-full h-full object-cover "
           playsInline
           loop={false}
           controls={true}
           muted
+          onLoadedData={() => console.log('video loaded')}
+          onPlay={() => console.log('video playing')}
+          onError={(e) => console.error('video error', e)}
         />
       </div>
 
       <button
-        onClick={togglePlayPause}
-        className="mt-4 px-6 py-2 bg-gray-800 text-white rounded-md border-2 border-gray-400 text-sm font-bold hover:bg-gray-700 transition"
+        onClick={async () => {
+          try {
+            const result = await videoRef.current?.play();
+            console.log('play promise result:', result);
+          } catch (err) {
+            console.error('Play error:', err);
+          }
+        }}
       >
-        {isPlaying ? 'Pause' : 'Play'}
+        Play
       </button>
     </div>
   );
